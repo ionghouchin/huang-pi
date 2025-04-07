b4f71aa06e0a501860317176d64b3b8f9b25137e
ionghouchin <94176766+ionghouchin@users.noreply.github.com>
2025-04-07 08:41:07 +0800

    Update data.js

---
+++ js/data.js
@@ -0,0 +1,30 @@
+const bioData = {
+    plants: [
+        {
+            id: 1,
+            name: "黃皮樹",
+            category: "plants",
+            image: "./images/1.jpg",
+            images: [
+                "./images/1.jpg",
+                "./images/89c4b7da334728efb81bc95e764c5705.JPG",
+                "./images/602dbaf61033f5fb0ecce509283dd555.JPG",
+                "./images/1350d3ea2e0ac9daf22cfd4a793457d1.JPG"
+            ],
+            description: "小喬木，高3-10米，小枝無毛，棕褐色，單數羽狀複葉，對生；小葉7-9片，紙質，卵狀披針形。黄皮樹先端長漸尖，基部圓形或寬楔形，兩側不對稱，表面幾乎無毛或在中脈被疏長毛，中脈基部毛較密，邊緣有細圓鋸齒，小葉柄長2-7毫米，腹面被疏短毛。",
+            distribution: "分佈於中國南方地區",
+            features: "樹高3.8米，樹幹圍48厘米。葉片長9.5厘米，寬5厘米，通常有5-11片。花穗長30厘米，花直徑1.5厘米。花具芳香，顏色為白色。樹皮可以入藥。",
+            scientificName: "Phellodendron sinii Y. C. Wu",
+            classification: {
+                kingdom: "植物界",
+                phylum: "被子植物門",
+                class: "木蘭綱",
+                order: "無患子目",
+                family: "芸香科",
+                genus: "黃檗屬",
+                species: "黃皮樹"
+            },
+            habitat: "生長於低海拔山區"
+        }
+    ]
+};
